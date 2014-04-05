7c45cfa5fa561e2e95c405dee04bf0d5f2af6921
Sander van der Burg <svanderburg@gmail.com>
2014-04-05 20:56:07 +0200

    Add hydra release expression

---
+++ release.nix
@@ -0,0 +1,48 @@
+{ nixpkgs ? <nixpkgs>
+, systems ? [ "i686-linux" "x86_64-linux" ]
+, libiffJobset ? import ../libiff/release.nix { inherit nixpkgs systems officialRelease; }
+, lib8svxJobset ? import ../lib8svx/release.nix { inherit nixpkgs systems officialRelease; }
+, SDL_8SVX ? { outPath = ./.; rev = 1234; }
+, officialRelease ? false
+
+}:
+
+let
+  pkgs = import nixpkgs {};
+  
+  version = builtins.readFile ./version;
+  
+  jobs = rec {
+    tarball =
+      with pkgs;
+
+      let
+        libiff = builtins.getAttr (builtins.currentSystem) (libiffJobset.build);
+        lib8svx = builtins.getAttr (builtins.currentSystem) (lib8svxJobset.build);
+      in
+      releaseTools.sourceTarball {
+        name = "SDL_8SVX-tarball";
+        src = SDL_8SVX;
+        inherit version officialRelease;
+
+        buildInputs = [ pkgconfig libiff lib8svx libresample SDL ];
+      };
+      
+    build =
+      pkgs.lib.genAttrs systems (system:
+        with import nixpkgs { inherit system; };
+        
+        let
+          libiff = builtins.getAttr system (libiffJobset.build);
+          lib8svx = builtins.getAttr system (lib8svxJobset.build);
+        in
+        releaseTools.nixBuild {
+          name = "SDL_8SVX";
+          inherit version;
+          src = tarball;
+          buildInputs = [ pkgconfig libiff lib8svx libresample SDL ];
+        }
+      );
+  };
+in
+jobs
